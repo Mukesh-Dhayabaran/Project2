fb93216ac358e2d706ee349a9faa26c5e18f373a
mukeshdhayabaran@gmail.com <mukeshdhayabaran@gmail.com>
2024-10-19 15:22:03 +0530

    Files added from mukesh branch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Blocks from './mainblock/block';
+import { Box } from '@mui/material';
+
+// const back={
+//   backgroundColor:'#F5F7FA',
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Box sx={{...back}}> */}
+      <Blocks/>
+    {/* </Box> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  
 reportWebVitals();
---
+++ src/mainblock/import.jsx
@@ -0,0 +1,5 @@
+import Box from '@mui/material/Box' 
+import Divider from '@mui/material/Divider' 
+import Button from '@mui/material/Button' 
+
+export {Box,Divider,Button}
